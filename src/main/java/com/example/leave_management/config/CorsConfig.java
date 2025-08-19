b5aa1da3bb63e0aa4b87922f36bac7ecd3af7dcd
praneethk000 <praneethkpoojary000@gmail.com>
2025-08-20 00:18:38 +0530

    Added docker file

--- src/main/java/com/example/leave_management/config/CorsConfig.java
+++ src/main/java/com/example/leave_management/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:3000") // Allow specific frontend origin
+                        .allowedOrigins("https://leave-management-frontend-six.vercel.app") // Allow specific frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies/auth headers if needed
